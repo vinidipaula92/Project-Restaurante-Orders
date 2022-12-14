00a55366f807666863a00944c42571dc68868d1b
vinidipaula92 <vinidipaula92@gmail.com>
2022-12-14 17:32:33 -0300

    feat: :sparkles: Implementação de leitura de arquivo CSV
    Com seus respectivos erros se não encontrar o arquivo e se a extensão for inválida

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,13 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-# new branch
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f'Extensão inválida: "{path_to_file}"')
+    try:
+        with open(path_to_file) as f:
+            reader = csv.reader(f)
+            file_reader = [row for row in reader]
+            return file_reader
+    except (FileNotFoundError):
+        raise FileNotFoundError(f'Arquivo inexistente: "{path_to_file}"')
