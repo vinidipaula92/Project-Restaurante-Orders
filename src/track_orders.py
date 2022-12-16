abc58447ba8e248d3d73e27b3219d1fbfe27a448
vinidipaula92 <vinidipaula92@gmail.com>
2022-12-16 16:01:17 -0300

    feat: :sparkles: Implementação da Classe TrackOrders
    A classe está devidamente modularizada, com todas as funções implementadas

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,38 @@
+from src.analyze_log import (
+    most_ordered_food,
+    never_ordered_food,
+    day_never_visited,
+    day_never_visited,
+    day_more_visited,
+    day_less_visited,
+)
+
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+
+    def __init__(self) -> None:
+        self._data = []
+        self._len = 0
+
     def __len__(self):
-        pass
+        return len(self._data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._data.append([customer, order, day])
+        self._len += 1
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return most_ordered_food(self._data, customer)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return never_ordered_food(self._data, customer)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return day_never_visited(self._data, customer)
 
     def get_busiest_day(self):
-        pass
+        return day_more_visited(self._data)
 
     def get_least_busy_day(self):
-        pass
+        return day_less_visited(self._data)
